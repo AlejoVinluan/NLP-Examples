b3384c4769c2d3250f5dd20b5ee69e1c2585c9cf
AlejoVinluan <alejo.vinluan146@gmail.com>
2023-02-09 11:57:34 -0600

    Fixed file read

--- Program 2/Main.py
+++ Program 2/Main.py
@@ -1,23 +1,21 @@
 import sys
-import os
-from functions import cleanData, getLexicalDiversity
+from functions import clean_data, get_lexical_diversity
 
 class Main:
     if len(sys.argv) <= 1:
         print("Wrong number of arguments. Please run by using python3 Main <file_name.txt>")
         exit(1)
     
-    text = ""
-        
-    print(os.getcwd())
-    print(os.listdir('.'))
-        
+    text_arr = []
+                
     try:
-        f = open(sys.argv[1], 'r')
-        raw_data = f.readlines()
-        text = cleanData(raw_data)
+        raw_data = open(sys.argv[1], 'r')
+        data_arr = raw_data.readlines()
+        text_arr = clean_data(data_arr)
     except:
         print("Unable to find file", sys.argv[1])
         exit(1)
     
-    print(text)
+    print(text_arr)
+    print(get_lexical_diversity(text_arr))
+    
--- Program 2/functions.py
+++ Program 2/functions.py
@@ -1,8 +1,13 @@
 import re
 
-def cleanData(text):
+def clean_data(arr):
+    text = ' '.join(arr)
+    text = text.lower()
     # Need to replace periods EXCEPT for when it's between numbers
-    return re.sub("[^a-zA-Z0-9 .,]|(?<!\\d)[.,]|[.,](?!\\d)", "", text)
+    regex = r"(\d+(?:\.\d+))|\W+"
+    text = re.sub(regex, lambda x: x.group(1) if x.group(1) else " ", text)
+    return text.split(' ')
 
-def getLexicalDiversity(text):
-    return len(set(text)) / len(text)
+def get_lexical_diversity(arr):
+    text = ' '.join(arr)
+    return len(set(text)) / len(arr)
