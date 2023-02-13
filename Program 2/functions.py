acd83b4b203373ad292a1dfe7b84ad1882ea9d5e
AlejoVinluan <alejo.vinluan146@gmail.com>
2023-02-13 15:01:58 -0600

    Finished steps 1-3

--- Program 2/functions.py
+++ Program 2/functions.py
@@ -1,13 +1,41 @@
-import re
+import copy
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 
-def clean_data(arr):
-    text = ' '.join(arr)
-    text = text.lower()
-    # Need to replace periods EXCEPT for when it's between numbers
-    regex = r"(\d+(?:\.\d+))|\W+"
-    text = re.sub(regex, lambda x: x.group(1) if x.group(1) else " ", text)
-    return text.split(' ')
+def pre_process(raw_text):
+    # Tokenize the lowercase, raw text
+    tokenized_lower = word_tokenize(raw_text.lower())
 
-def get_lexical_diversity(arr):
-    text = ' '.join(arr)
-    return len(set(text)) / len(arr)
+    # Reduce tokens to only alpha
+    tokenized_arr = [word for word in tokenized_lower if word.isalpha()]
+
+    # Remove any stopwords located in array
+    english_stopwords = set(stopwords.words('english'))
+    tokenized_arr = [word for word in tokenized_arr if word not in english_stopwords]
+    
+    # Remove any words with length less than or equal to 5
+    tokenized_arr = [word for word in tokenized_arr if len(word) > 5]
+
+    # Lemmatize Tokens
+    tokenized_arr = [WordNetLemmatizer().lemmatize(word) for word in tokenized_arr]
+    tokens_arr = copy.deepcopy(tokenized_arr)
+    num_tokens = len(tokenized_arr)
+
+    # Create a set of tokens to ensure uniqueness
+    tokenized_arr = set(tokenized_arr)
+    
+    # POS-Tag the lemmas and print the first 20 tagged
+    pos_tagged_arr = pos_tag(tokenized_arr)
+    print("Part of Speech Tagged Lemmas (first 20) - ")
+    print(pos_tagged_arr[:20],"\n")
+
+    # Extract all nouns
+    nouns = [noun[0] for noun in pos_tagged_arr if noun[1] == "NN"]
+    num_nouns = len(nouns)
+
+    print("Number of tokens:", num_tokens)
+    print("Number of nouns:", num_nouns)
+
+    return tokens_arr, nouns
