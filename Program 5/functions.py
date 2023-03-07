1483d3211baddc7954ee611fdb43060ac9775dc0
AlejoVinluan <alejo.vinluan146@gmail.com>
2023-03-07 16:02:08 -0600

    Started program 5

--- Program 5/functions.py
+++ Program 5/functions.py
@@ -1,14 +1,60 @@
-# Returns a list of URL's from the base URL
+import requests
+import random
+import os
+from bs4 import BeautifulSoup
+
+# Returns a soup object of a given url
+def get_soup(url):
+    page = requests.get(url)
+    return BeautifulSoup(page.text, 'html.parser')
+
+# Returns 1 list of 15 total URL's
 def scrape_base(url):
-    return 0
+    soup = get_soup(url)
+    base_url = url.split("/")[2]
+
+    # Gets different links within the Wikipedia page
+    stat_links = soup.find_all('a')
+    wiki_links = set()
+    external_links = set()
+    for raw_link in stat_links:
+        link = raw_link.get('href')
+        if not link or 'file' in link.lower() or link.startswith('#'):
+            continue
+        if link.startswith('/wiki/'):
+            wiki_links.add('https://' + base_url + link)
+        elif 'wiki' not in link.lower() and link.lower().startswith('https://'):
+            external_links.add(link)
+    return create_list(wiki_links, external_links)
         
+# Creates a 15 sized sample from two lists, 10 from list1, 5 from list2
+def create_list(list1, list2):
+    random.seed(123)
+    main_list = random.sample(list1,10)
+    list2_sample = random.sample(list2,5)
+    for sample in list2_sample:
+        main_list.append(sample)
+    return main_list
+
 # Makes a file for the url inputted
-def make_file(url):
-    return 0
+def extract_and_make_file(url, file_name):
+    soup = get_soup(url)
+    text = soup.get_text()
+    with open("text_files/" + file_name, "w") as file:
+        file.write(text)
     
 # Clean up text. Extract from NLTK tokenizer
 def clean_up_text(file):
-    return 0
+    with open(file, 'r') as f:
+        text_arr = f.readlines()
+    main_text = ""
+    for text in text_arr:
+        text.strip()
+        if len(text) > 0:
+            main_text = main_text + text
+    print(main_text)
+    with open(file, 'w') as f:
+        f.write(main_text)
         
 # Extract 3 important terms from file
 def extract_terms(file):
--- Program 5/main.py
+++ Program 5/main.py
@@ -1,6 +1,32 @@
-import requests
-from bs4 import BeautifulSoup
-from functions import scrape_base, make_file, clean_up_text, extract_terms
+import os
+from functions import scrape_base, extract_and_make_file, clean_up_text
 
 class Main:
-    print('Hello')
+    # Start with the main website
+    nba_wiki = 'https://en.wikipedia.org/wiki/National_Basketball_Association'
+
+    # Create a set of links extracted from main website
+    link_set = scrape_base(nba_wiki)
+    
+    # Make a subdirectory for the created files
+    if not os.path.exists("text_files"):
+        os.makedirs("text_files")
+
+    # Extract all text from a webpage and store text in file
+    #  idx used to create unique filename for text
+    idx = 0
+    for link in link_set:
+        file_name = "file" + str(idx) + ".txt"
+        extract_and_make_file(link, file_name)
+        idx += 1
+
+    text_files_dir = os.listdir('text_files')
+    clean_up_text("text_files/file0.txt")
+    #for file_name in text_files_dir:
+        #clean_up_text("text_files/" + file_name)
+    
+
+
+    
+    
+    
