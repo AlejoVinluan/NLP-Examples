cf45e0c6b58cf46060d3c14e9cd4a1d1f2f4002c
AlejoVinluan <alejo.vinluan146@gmail.com>
2023-03-07 12:26:56 -0600

    Started program 5

---
+++ Program 5/functions.py
@@ -0,0 +1,18 @@
+# Returns a list of URL's from the base URL
+def scrape_base(url):
+    return 0
+        
+# Makes a file for the url inputted
+def make_file(url):
+    return 0
+    
+# Clean up text. Extract from NLTK tokenizer
+def clean_up_text(file):
+    return 0
+        
+# Extract 3 important terms from file
+def extract_terms(file):
+    # Lower case and remove sotpwords
+    
+    return 0
+    
---
+++ Program 5/main.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+from functions import scrape_base, make_file, clean_up_text, extract_terms
+
+class Main:
+    print('Hello')
