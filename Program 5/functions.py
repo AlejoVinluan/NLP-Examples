18439d5c52b112f7c5d8b66d787529897cba8d8f
AlejoVinluan <alejo.vinluan146@gmail.com>
2023-03-09 02:05:36 -0600

    Completed assignment

--- Program 5/functions.py
+++ Program 5/functions.py
@@ -1,6 +1,8 @@
 import requests
 import random
 import os
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
 from bs4 import BeautifulSoup
 
 # Returns a soup object of a given url
@@ -19,7 +21,8 @@ def scrape_base(url):
     external_links = set()
     for raw_link in stat_links:
         link = raw_link.get('href')
-        if not link or 'file' in link.lower() or link.startswith('#'):
+        # Filter for websites that don't work well in this assignment
+        if not link or 'file' in link.lower() or link.startswith('#') or 'archive' in link.lower() or 'slate' in link.lower():
             continue
         if link.startswith('/wiki/'):
             wiki_links.add('https://' + base_url + link)
@@ -30,8 +33,8 @@ def scrape_base(url):
 # Creates a 15 sized sample from two lists, 10 from list1, 5 from list2
 def create_list(list1, list2):
     random.seed(123)
-    main_list = random.sample(list1,10)
-    list2_sample = random.sample(list2,5)
+    main_list = random.sample(sorted(list1),10)
+    list2_sample = random.sample(sorted(list2),5)
     for sample in list2_sample:
         main_list.append(sample)
     return main_list
@@ -40,25 +43,51 @@ def create_list(list1, list2):
 def extract_and_make_file(url, file_name):
     soup = get_soup(url)
     text = soup.get_text()
-    with open("text_files/" + file_name, "w") as file:
+    with open("text_files/" + file_name, "w", encoding="utf-8") as file:
         file.write(text)
     
 # Clean up text. Extract from NLTK tokenizer
 def clean_up_text(file):
-    with open(file, 'r') as f:
-        text_arr = f.readlines()
-    main_text = ""
-    for text in text_arr:
-        text.strip()
-        if len(text) > 0:
-            main_text = main_text + text
-    print(main_text)
-    with open(file, 'w') as f:
-        f.write(main_text)
+    clean_text = ""
+    with open(file, 'r', encoding="utf-8") as f:
+        for line in f:
+            if not line.isspace():
+                clean_text += line
+    tokenized_sentences = sent_tokenize(clean_text)
+    final_text = ""
+    for sentence in tokenized_sentences:
+        final_text = final_text + sentence + "\n"
+    file_name = file.split('/')[1]
+    # Write to a new file in clean_files directory
+    with open("clean_files/clean_" + file_name, 'w', encoding="utf-8") as f:
+        f.write(final_text)
+
         
-# Extract 3 important terms from file
-def extract_terms(file):
-    # Lower case and remove sotpwords
-    
-    return 0
+def extract_terms(file_directory):
+    clean_file_dir = os.listdir(file_directory)
+    filtered_words = []
+    # Loop through each file and add all words to filtered_words array
+    for file in clean_file_dir:
+        raw_arr = []
+        with open(file_directory + "/" + file, 'r', encoding="utf-8") as f:
+            raw_arr = f.readlines()
+        raw_text = " ".join(raw_arr)
+        tokenized_words = word_tokenize(raw_text.lower())
+        stop_words = set(stopwords.words('english'))
+         # Lower case and remove stopwords
+        filtered_arr = [word for word in tokenized_words if word.isalpha() and word not in stop_words and len(word) > 1]
+        for word in filtered_arr:
+            filtered_words.append(word)
+            
+        # Find frequency of all words
+        word_frequency = {}
+        for word in filtered_words:
+            if word in word_frequency:
+                word_frequency[word] += 1
+            else:
+                word_frequency[word] = 1
+    # Sort the words in dict by frequency, with count descending
+    sorted_words = sorted(word_frequency.items(), key=lambda x:x[1], reverse=True)
+    # Return first 40 words of dict
+    return sorted_words[:40]
     
--- Program 5/main.py
+++ Program 5/main.py
@@ -1,5 +1,6 @@
 import os
-from functions import scrape_base, extract_and_make_file, clean_up_text
+import pickle
+from functions import scrape_base, extract_and_make_file, clean_up_text, extract_terms
 
 class Main:
     # Start with the main website
@@ -15,16 +16,51 @@ class Main:
     # Extract all text from a webpage and store text in file
     #  idx used to create unique filename for text
     idx = 0
+
+    # Create a file for each link from original website
     for link in link_set:
         file_name = "file" + str(idx) + ".txt"
         extract_and_make_file(link, file_name)
         idx += 1
-
-    text_files_dir = os.listdir('text_files')
-    clean_up_text("text_files/file0.txt")
-    #for file_name in text_files_dir:
-        #clean_up_text("text_files/" + file_name)
+        
+    # Create directory if it does not exist
+    if not os.path.exists("clean_files"):
+        os.makedirs("clean_files")
     
+    # Clean text from all text files originally created
+    text_files_dir = os.listdir('text_files')
+    for file_name in text_files_dir:
+        clean_up_text("text_files/" + file_name)
+        
+    # Extract the important terms from the cleaned files
+    important_terms = extract_terms("clean_files/")
+    # Print the important terms and their occurence
+    for idx, term in enumerate(important_terms):
+        print("Rank:", idx+1)
+        print("Term:", term[0])
+        print("Occurrences:", term[1], "\n")
+    
+    # Create a knowledge base from the top 10 terms by hand
+    knowledge_base = {
+        "team": "a group of players forming one side in a competitive game or sport",
+        "league": "a collection of people, countries, or groups that combine for a particular purpose, typically mutual protection or cooperation",
+        "nba": "the National Basketball Assosciation",
+        "season": "a fixed time in the year when a particular sport is played",
+        "wnba": "the Womens National Basketball Assosciation",
+        "players": "a person taking part in a sport or game",
+        "basketball": "a game played between two teams of five players in which goals are scored by throwing a ball through a netted hoop fixed above each end of the court",
+        "sport": "an activity involving physical exertion and skill in which an individual or team competes against another or others for entertainment",
+        "game": "a form of play or sport, especially a competitive one played according to rules and decided by skill, strength, or luck",
+        "may": "The month where the NBA Playoffs and NBA Finals generally occur"
+    }
+    
+    # Print each key and definition of the knowledge base
+    for key in knowledge_base:
+        print("Term:", key)
+        print("Definition:", knowledge_base[key], "\n")
+    
+    # Create a pickle of the dictionary for the knowledge base
+    pickle.dump(knowledge_base, open('knowledge_base.p', 'wb'))
 
 
     
